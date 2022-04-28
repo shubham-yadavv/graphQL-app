d71d6ccba13952f70eb60fc87cdd8e03120498f2
shubham <shubham22121@gmail.com>
2022-04-29 02:56:09 +0530

    graphql apollo server

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const { ApolloServer, PubSub } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
-const pubsub = new PubSub();
-
-require('dotenv').config();
-const port = process.env.PORT || 4000;
 const uri = process.env.ATLAS_URI;
+const port = process.env.PORT || 8080;
+
+
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req, pubsub })
+    typeDefs,
+    resolvers
 });
 
 mongoose
-  .connect(uri, { useNewUrlParser: true })
-  .then(() => {
-    console.log('MongoDB Connected');
-    return server.listen({ port: port });
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  })
-  .catch(err => {
-    console.error(err)
-  })
+    .connect(uri, { useNewUrlParser: true })
+    .then(() => {
+        console.log('MongoDB Connected');
+        return server.listen({ port: port });
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`);
+    })
+    .catch(err => {
+        console.error(err)
+    })
--- models/Post.js
+++ models/Post.js
@@ -1,26 +1,22 @@
 const { model, Schema } = require('mongoose');
 
 const postSchema = new Schema({
-  body: String,
-  username: String,
-  createdAt: String,
-  comments: [
-    {
-      body: String,
-      username: String,
-      createdAt: String
+    body: String,
+    username: String,
+    createdAt: String,
+    comments: [{
+        body: String,
+        username: String,
+        createdAt: String,
+    }],
+    likes: [{
+        username: String,
+        createdAt: String,
+    }],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
     }
-  ],
-  likes: [
-    {
-      username: String,
-      createdAt: String
-    }
-  ],
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
-  }
 });
 
 module.exports = model('Post', postSchema);
