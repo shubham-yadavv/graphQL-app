9f64a0f1dc6b8e0fc36ed0632c9565d9011da3f5
shubham <shubham22121@gmail.com>
2022-05-04 00:45:13 +0530

    created get posts resolver

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,6 @@
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
     Query: {
@@ -10,5 +12,31 @@ module.exports = {
                 throw new Error(err);
             }
         }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            const user = checkAuth(context);
+
+            if (body.trim() === '') {
+                throw new Error('Post body must not be empty');
+            }
+
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+
+            context.pubsub.publish('NEW_POST', {
+                newPost: post
+            });
+
+            return post;
+        }
+
     }
+
 }
